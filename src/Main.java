94d293b67c6ca6cfb3f5053ffb97f3da0da4cb57
ericatkins8 <eric.atkins@hilliardschools.org>
2022-09-22 12:50:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("What is number 1?");
+        int num1 = 5;
+        System.out.println("What is number 2?");
+        int num2 = 8;
+
+        if (num1 > num2)
+            System.out.println("number 1 is larger, number 2 is smaller");
+        else if (num1 == num2)
+            System.out.println("number 1 and number 2 are equal");
+        else if (num2 > num1)
+            System.out.println("number 2 is larger, number 1 is smaller");
+        else
+            System.out.println("numbers 1 and/or 2 are invalid.");
+    }
+}
